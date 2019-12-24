4bce9d2534fcff9ff9396ea4963f0a54db5a6f96
Monte Johnston <johnston.monte@gmail.com>
2019-12-24 13:08:14 -0500

    don't mess with database just yet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 mix deps.get
 mix deps.compile # get updated dependencies & compile them
-mix do ecto.create, ecto.migrate(PROJECT) 
+# mix do ecto.create, ecto.migrate
 cd assets && npm install && npm run deploy & cd ..
 mix phx.digest
 mix compile
