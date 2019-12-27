e07ae86b43709679378243ed076089e2896b0a3b
Monte Johnston <johnston.monte@gmail.com>
2019-12-26 20:08:50 -0500

    change directory in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
+
+cd ~/worte 
 git pull
 
- mix deps.get --only prod
-$ MIX_ENV=prod mix compile
+mix deps.get --only prod
+MIX_ENV=prod mix compile
 
 npm run deploy --prefix ./assets
 # mix do ecto.create, ecto.migrate
 
 # MIX_ENV=prod mix ecto.migrate
-cd assets && npm install && npm run deploy & cd ..
+# cd assets && npm install && npm run deploy & cd ..
 mix phx.digest
 mix compile
 MIX_ENV=prod mix release
