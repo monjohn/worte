59615776c956c41e2bd23f1083e49faa40cfa436
Monte Johnston <johnston.monte@gmail.com>
2019-12-27 11:00:42 -0500

    use real app name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,19 @@
 #! /usr/bin/env bash
 . ~/.bashrc
-
 cd ~/worte 
-git pull
 
-mix --version
+git pull
 
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 npm run deploy --prefix ./assets
-# mix do ecto.create, ecto.migrate
 
+# mix do ecto.create, ecto.migrate
 # MIX_ENV=prod mix ecto.migrate
 # cd assets && npm install && npm run deploy & cd ..
 mix phx.digest
 mix compile
 MIX_ENV=prod mix release --overwrite
 
-_build/prod/rel/my_app/bin/my_app daemon_iex
+_build/prod/rel/my_app/bin/worte daemon_iex
