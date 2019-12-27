34e75c77a480a7ace0e2bb3c7504579d38733c0e
Monte Johnston <johnston.monte@gmail.com>
2019-12-26 19:28:33 -0500

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
-mix deps.get
-mix deps.compile # get updated dependencies & compile them
+git pull
+
+ mix deps.get --only prod
+$ MIX_ENV=prod mix compile
+
+npm run deploy --prefix ./assets
 # mix do ecto.create, ecto.migrate
+
+# MIX_ENV=prod mix ecto.migrate
 cd assets && npm install && npm run deploy & cd ..
 mix phx.digest
 mix compile
 MIX_ENV=prod mix release
+
+PORT=4001 MIX_ENV=prod elixir --erl "-detached" -S mix phx.server
